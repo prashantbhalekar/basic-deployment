23b7286e2be33356c89b56dcb7ff7bb66d17e45b
Prashant Bhalekar <prashantbhalekar474@gmail.com>
2026-08-01 16:11:39 +0530

    feat: updated ec2 host for git hub action

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const express = require("express");
 const app = express(express.json());
 
 app.get("", (req, res) => {
-  res.send("Updated Deployment with Base Deployment to Github Action!!!");
+  res.send(
+    "Updated Deployment with Base Deployment to Github Action!!!\n\nDeployed Again!!!",
+  );
 });
 
 app.get("/json", (req, res) => {
